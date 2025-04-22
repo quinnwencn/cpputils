19b85d51246dc16cd480b3ce1e8c8a7716132a4e
Quinn <quinnwen.cn@gmail.com>
2025-04-22 22:34:52 +0800

    Init first commit
    1. Add utils objects but not tested yet
    2. Add conan and cmake scripts
    
    Signed-off-by: Quinn <quinnwen.cn@gmail.com>

---
+++ conanfile.py
@@ -0,0 +1,34 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout, CMakeToolchain
+
+
+class CppUtil(ConanFile):
+    name = "cpputils"
+    version = "0.1.0"
+    description = "cpputils is a cpp library that provides basic functions."
+    settings = "os", "compiler", "build_type", "arch"
+    generator = "CMakeDeps", "CMakeToolchain"
+    exports_sources = "CMakeLists.txt", "include/", "src/*"
+
+    def build_requirements(self):
+        self.build_requires("cmake/3.26.3")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+    
+    def build(self):
+        cmake = CMake(self)
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["cpputils"]
+        self.cpp_info.includedirs = ["include"]
+        self.cpp_info.set_property("cmake_target_name", "cpputils::cpputils")
---
+++ include/cpputils/utils.h
@@ -0,0 +1,10 @@
+#include <string_view>
+#include <string>
+#include <vector>
+#include <cstdint>
+
+namespace Utils {
+
+std::string ByteArr2HexStr(const std::vector<uint8_t>& arr);
+
+}
